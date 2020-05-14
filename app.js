972d15f1fd27e4c66be1387ffee0c78e59954a98
Gabriele Concli <gabriele.concli@gmail.com>
2020-05-14 09:43:29 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function exec() {
+  squares();
+}
+
+function squares() {
+  for (var i = 1; i <= 15; i++) {
+    document.writeln(i * i + '<br>');
+  }
+  document.writeln('fine elaborazione');
+}
